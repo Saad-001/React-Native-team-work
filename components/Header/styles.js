97edb2e257d34026f22ba80ee4c889f5dcceeff8
Saad-001 <saadafnan21@gmail.com>
2021-06-04 18:18:41 +0600

    fixed some issues

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -1,23 +1,3 @@
-styles.js
-
-Type
-Javascript
-Size
-463 bytes
-Storage used
-463 bytes
-Location
-anikvai
-Owner
-Sa'ad
-Modified
-2:09 PM by Sa'ad
-Opened
-4:24 PM by me
-Created
-4:20 PM
-No description
-Viewers can download
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
